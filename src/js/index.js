ec46879d8ae25c6b885168537e66a309bb517fcc
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-21 04:28:45 +0100

    Finish project

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import { Router } from "./router.js"
+
+const router = new Router()
+
+router.add("/", "/src/pages/home.html")
+router.add("/universe", "/src/pages/universe.html")
+router.add("/explore", "/src/pages/explore.html")
+router.add(404, "/src/pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
+
